0b90912f7eba624d357658d1a13e1c27aa09fe86
Unco <wizard2070@gmail.com>
2023-08-16 15:55:00 +0430

    3rd

---
+++ src/backgroundMessaging.js
@@ -0,0 +1,20 @@
+chrome.bookmarks.onMoved.addListener(() => {
+    alert('Bookmark moved :D');
+
+    chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+        chrome.tabs.sendMessage(
+            tabs[0].id,
+            { name: 'Ali' },
+            () => console.log('background_callback')
+        );
+    });
+});
+
+chrome.runtime.onMessage.addListener((req, sender, callback) => {
+    // also should sanitize input
+    console.log('req ', req);
+    console.log('sender ', sender);
+    console.log('callback ', callback());
+    console.log('errors ', chrome.runtime.lastError);
+})
+
