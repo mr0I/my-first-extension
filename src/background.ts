36f7b32ea70d3aa395660c54a61ba30cf45017f4
Unco <wizard2070@gmail.com>
2023-08-19 11:49:19 +0430

    6th

--- src/background.ts
+++ src/background.ts
@@ -1,3 +1,5 @@
+/** do not use global variables here */
+
 chrome.runtime.onInstalled.addListener(() => {
     console.log('Extension installed...');
 });
@@ -5,3 +7,28 @@ chrome.runtime.onInstalled.addListener(() => {
 chrome.bookmarks.onCreated.addListener(() => {
     alert('Bookmark created :D');
 });
+
+chrome.action.onClicked.addListener((tab) => {
+    console.log(tab);
+});
+
+chrome.bookmarks.onMoved.addListener(() => {
+    alert('Bookmark moved :D');
+
+    chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+        chrome.tabs.sendMessage(
+            tabs[0].id,
+            { name: 'Ali' },
+            (res) => console.log('background_callback')
+        );
+    });
+});
+
+chrome.runtime.onMessage.addListener((req, sender, callback) => {
+    // also should sanitize input
+    console.log('req ', req);
+    console.log('sender ', sender);
+    console.log('callback ', callback('hellooooooo'));
+    console.log('errors ', chrome.runtime.lastError);
+})
+
